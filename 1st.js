80d20fe4b1f47f9807ab7689e443caa1efa0fd32
Aimon-Rana-Jihad <157241369+Aimon-Rana-Jihad@users.noreply.github.com>
2024-02-28 01:22:17 +0600

    1st assignment

---
+++ 1st.js
@@ -0,0 +1,36 @@
+// 1
+
+// Num1 = 59,
+// txt1 = "hello JS"
+// console.log(Num1==txt1)
+
+// 2
+
+// Num2 = 344,
+//  txtNum = "344"
+//  console.log(Num2===txtNum)
+
+// 3
+
+// var oddNum = 75;
+// var evenNum = 90;
+// var boolValue = oddNum !== evenNum;
+// console.log(boolValue);
+// true
+
+// 4
+
+// var a = "Aimon"
+// var b = " Rana"
+// console.log(a+b)
+
+// 5
+
+// var a = 5
+// var b = "60"
+// console.log(a+Number(b))
+
+
+
+
+
